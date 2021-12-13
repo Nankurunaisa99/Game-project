11c41debf77fac8a63dc393e4a11c0521af2f184
Nankurunaisa99 <speranzabiagio99@gmail.com>
2021-12-13 01:42:59 +0100

    Meccanica del movimento .2
    Nessuna modifica degna di nota: ho implementato per ora in modo grezzo il sistema che ti permette di  riconoscere quale regione dello schermo è stata toccata e ho iniziato a buttare giù qualche idea sulla gestione del cambiamento di host a runtime (meccanica dello switch)

--- Soul Project/Assets/Scripts/HostController.cs
+++ Soul Project/Assets/Scripts/HostController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class HostController : MonoBehaviour
 {
     public Host host;
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D hostRb2d;
 
     [HideInInspector]
     public bool isControlled = false;
@@ -19,19 +21,32 @@ public class HostController : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
+    {   
         hostName = host.hostName;
         movementVelocity = host.movementVelocity;
         jumpForce = host.jumpForce;
         animatorOverrideController = host.animatorOverrideController;
-        SpriteRenderer hostSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-        hostSpriteRenderer.sprite = host.hostSprite;
 
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = host.hostSprite;
+
+        hostRb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(hostInputScript.isLeftScreenPressed == false && hostInputScript.isRightScreenPressed == true) 
+        {
+            spriteRenderer.color = Color.red;
+        }
+        else if(hostInputScript.isLeftScreenPressed == true && hostInputScript.isRightScreenPressed == false) 
+        {
+            spriteRenderer.color = Color.yellow;
+        }
+        else
+        {
+            spriteRenderer.color = Color.blue;
+        }
     }
 }
--- Soul Project/Assets/Scripts/HostInputScript.cs
+++ Soul Project/Assets/Scripts/HostInputScript.cs
@@ -12,9 +12,18 @@ public class HostInputScript : MonoBehaviour
 
     float thirdScreenWidth;
 
+    int leftFingerID;
+    int rightFingerID;
+
     // Start is called before the first frame update
     void Start()
-    {   
+    {
+        leftFingerID = -1;
+        rightFingerID = -1;
+
+        isLeftScreenPressed = false;
+        isRightScreenPressed = false;
+
         //Divido lo schermo del cellulare in 3 in modo da tener traccia delle regioni in cui è stato effettuato il tocco
         thirdScreenWidth = Screen.width / 3;
     }
@@ -22,6 +31,49 @@ public class HostInputScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        for(int i = 0; i < Input.touchCount; i++)
+        {
+            Touch t = Input.GetTouch(i);
+
+            switch (t.phase)
+            {
+                case TouchPhase.Began:
+
+                    if(t.position.x < thirdScreenWidth && leftFingerID == -1)
+                    {
+                        leftFingerID = t.fingerId;
+                        isLeftScreenPressed = true;
+                        Debug.Log("Left screen pressed");
+
+                    }
+                    else if(t.position.x > 2*thirdScreenWidth && rightFingerID == -1)
+                    {
+                        rightFingerID = t.fingerId;
+                        isRightScreenPressed = true;
+                        Debug.Log("Right screen pressed");
+                    }
+                    break;
+
+                case TouchPhase.Ended:
+                case TouchPhase.Canceled:
+
+                    if(t.fingerId == leftFingerID)
+                    {
+                        leftFingerID = -1;
+                        isLeftScreenPressed = false;
+                        Debug.Log("Left screen not pressed anymore");
+                    }
+                    else if(t.fingerId == rightFingerID)
+                    {
+                        rightFingerID = -1;
+                        isRightScreenPressed = false;
+                        Debug.Log("Right screen not pressed anymore");
+                        
+                    }
+                    break;
+
+            }
+        }
         
     }
 }
